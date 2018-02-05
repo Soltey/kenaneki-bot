6061b2d2c3de8d3ea9994c686c4963b8c63f141e
Soltey <36131464+Soltey@users.noreply.github.com>
2018-02-05 17:40:42 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
-const prefix = "-"
-
-bot.on("ready", () => {
-console.log("Ready as " + bot.user.username + "#" + bot.user.discriminator);
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
 bot.login("MzkzNzUwNDY0MzQ2MzI1MDA0.DR6USA.pkt2EkGTjus_kSB7Dd2ZQrJPfSs")
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
