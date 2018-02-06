4a21988c9a4eeece51d95911112690f1737e4bd7
Soltey <36131464+Soltey@users.noreply.github.com>
2018-02-06 17:49:36 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ console.log("Ready as " + bot.user.username + "#" + bot.user.discriminator);
 
 bot.login("MzkzNzUwNDY0MzQ2MzI1MDA0.DVnDkA.tSG6xF0j_GPBaE5ELqurZfhuOGU")
 
-bot.setGame('-help for help!!')
+bot.user.setGame('-help for help!!')
 
 bot.on("message", function(msg) {
     console.log(`${msg.author.tag} in #${msg.channel.name} = ${msg.content}`)
