86460406befe45e372de0ebd9af27cf88a295624
Soltey <36131464+Soltey@users.noreply.github.com>
2018-02-06 18:13:36 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ console.log("Ready as " + bot.user.username + "#" + bot.user.discriminator);
 
 bot.login("MzkzNzUwNDY0MzQ2MzI1MDA0.DVnDkA.tSG6xF0j_GPBaE5ELqurZfhuOGU")
 
-bot.user.setActivity('-help for help!!!', {type: 'STREAMING', url: 'https://twitch.tv/soltey%26'});
+bot.user.setGame('-help for help!!!', {type: 'STREAMING', url: 'https://twitch.tv/soltey%26'});
 
 bot.on("message", function(msg) {
     console.log(`${msg.author.tag} in #${msg.channel.name} = ${msg.content}`)
