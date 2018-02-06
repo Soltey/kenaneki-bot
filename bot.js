cdf5d145a6df29c60d050b4ded4c02c402f401da
Soltey <36131464+Soltey@users.noreply.github.com>
2018-02-06 18:07:40 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ console.log("Ready as " + bot.user.username + "#" + bot.user.discriminator);
 
 bot.login("MzkzNzUwNDY0MzQ2MzI1MDA0.DVnDkA.tSG6xF0j_GPBaE5ELqurZfhuOGU")
 
-<Client>.user.setActivity('-help for help!!!', { // Your status
+ bot.user.setActivity('-help for help!!!', { // Your status
  'type': 'STREAMING', // Change to WATCHING or LISTENING for those
- 'url': 'https://twitch.tv/soltey' // Remove this line and the comma in the line before
+ 'url': 'https://twitch.tv/twitch' // Remove this line and the comma in the line before
  });
 
 bot.on("message", function(msg) {
